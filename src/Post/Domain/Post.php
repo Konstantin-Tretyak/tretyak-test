9d4ae2059ca04fd4a60393384cb0c8e9ecbd5366
Konstantin Tretyak <kons1tretyak1valant@gmail.com>
2023-02-21 14:16:47 +0100

    Add new property description and saving this property in database

--- src/Post/Domain/Post.php
+++ src/Post/Domain/Post.php
@@ -24,20 +24,24 @@ class Post extends AggregateRoot
     #[ORM\Column(length: 255)]
     private string $summary;
 
-    public function __construct(string $id, string $title, string $summary)
+    #[ORM\Column(type: "text", nullable: true)]
+    private string $description;
+
+    public function __construct(string $id, string $title, string $summary, string $description)
     {
         $this->id = Uuid::fromString($id);
         $this->title = $title;
         $this->summary = $summary;
+        $this->description = $description;
     }
 
-    public static function new(string $id, string $title, string $summary): self
+    public static function new(string $id, string $title, string $summary, string $description): self
     {
         Assert::that($id)->uuid();
         Assert::that($title)->maxLength(20)->notBlank();
         Assert::that($summary)->maxLength(255)->notBlank();
 
-        return new self($id, $title, $summary);
+        return new self($id, $title, $summary, $description);
     }
 
     public function getId(): Uuid
